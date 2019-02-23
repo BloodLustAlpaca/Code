cc416f9868f38ab2d28fd5147961e8745fb857c2
BloodLustAlpaca <drummerboyas157@yahoo.com>
2019-02-23 11:01:16 -0700

    Update stocks.py
    got moving average working

--- MachineLearning/stockProject/stocks.py
+++ MachineLearning/stockProject/stocks.py
@@ -10,33 +10,37 @@ import zipfile
 from datetime import datetime, timedelta
 
 def run():
+    #ZF is the zip file will all the stocks and etfs broken up into the two folders
     zf = zipfile.ZipFile('Data.zip')
+    #ETFs or Stocks with /ticker.us.txt will refence the ticker needed
     df = pd.read_csv(zf.open('ETFs/qqq.us.txt'))
     print(df.columns)
     print(df.sample(n=3))
     
     print(movAvg([1,2,3,4,5]))
-    movAvg1("Open", "2010-10-22",10,"qqq")
+    print(movAvg1("Open", "2010-10-22",10,"qqq"))
     
 def movAvg1(open_or_close,startDate,days,ticker):
     zf = zipfile.ZipFile('Data.zip')
     df = pd.read_csv(zf.open('ETFs/' + ticker + '.us.txt'))
-    dfIndex = df[(df['Date'] == startDate)].index[0]
-    print(df.loc[dfIndex,'Date'])
-    print(type(df.loc[df.index[0],"Date"]))
-    print(dfIndex)
     date = startDate
+    counter = 0
     mvavg = 0
-    for x in range (0,days):
-        #find how to weekdays
-        #print('TEST:' + str(df.loc[df.Date == date , 'Open'].values[0]))
-        #while((df.loc[df.Date == date, 'Open']).empty):
-        #   date = (datetime.strptime(startDate, '%Y-%m-%d') + timedelta(days=x)).strftime('%Y-%m-%d')
-        mvavg += df.loc[df.Date == date , 'Open']      #currentDf = df[df.Date == date]
-        #mvavg += currentDf.Open.tolist()[0]
-        date = (datetime.strptime(startDate, '%Y-%m-%d') + timedelta(days=x)).strftime('%Y-%m-%d')
-        #int(np.isnan(df.loc[df.Date == date, 'Open'])
-        print()
+    daysInFuture = 0
+    
+    #counter is incremented when a valid day is read, weekends will be skipped
+    #date adds the days in the future for the next try
+    
+    while(counter < days):
+        daysInFuture += 1
+        try:
+            value = df.loc[df.Date == date , 'Open'].tolist()[0]
+            mvavg += value
+            counter += 1
+        except:
+            pass
+        date = (datetime.strptime(startDate, '%Y-%m-%d') + timedelta(daysInFuture)).strftime('%Y-%m-%d')
+    return mvavg/days
 def movAvg(arr):
     answer = 0
     for x in arr:
